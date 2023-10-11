b129e211e606a2aaa7c418ce3a667e6deba0293c
philippOkie <kriukphilipp@gmail.com>
2023-10-11 12:39:20 +0300

    almost working

--- src/components/uiModule.js
+++ src/components/uiModule.js
@@ -24,13 +24,12 @@ export class ShowBoard {
           cell.setAttribute("y", i);
           cell.setAttribute("x", j);
           cell.setAttribute("value", 0);
-          cell.addEventListener("click", (e) => {
+          cell.addEventListener("click", () => {
             runVar = cell.getAttribute("value");
             if (player.onesInBoard >= 53 && runVar == 0) {
               cell.classList.add("isWata");
               cell.setAttribute("value", 2);
-
-              const cellsPlayer = document.querySelectorAll(".cellPlayer");
+              attack();
               cellsPlayer.forEach((cp) => {
                 var value = cp.getAttribute("value");
                 if (value == -1) {
@@ -43,11 +42,12 @@ export class ShowBoard {
                       timerInSeconds += 1;
 
                       header.innerText = `Restart game in: ${timerInSeconds}`;
-
                       if (timerInSeconds >= 5) {
                         window.location.reload();
                       }
                     }, 1000);
+                  } else if (this.playerToLose !== 17) {
+                    this.playerToLose = 0;
                   }
                 }
               });
@@ -58,12 +58,12 @@ export class ShowBoard {
         } else if (compBoard.board[i][j] === 1) {
           const cell = document.createElement("div");
           cell.classList.add("cell");
-          cell.classList.add("isShip");
+          // cell.classList.add("isShip");
           computerBoard.appendChild(cell);
           cell.setAttribute("y", i);
           cell.setAttribute("x", j);
           cell.setAttribute("value", 1);
-          cell.addEventListener("click", (e) => {
+          cell.addEventListener("click", () => {
             runVar = cell.getAttribute("value");
             if (player.onesInBoard >= 53 && runVar == 1) {
               cell.classList.add("isHit");
@@ -83,6 +83,29 @@ export class ShowBoard {
                   }
                 }, 1000);
               }
+              // const cellsPlayer = document.querySelectorAll(".cellPlayer");
+              // attack();
+              // cellsPlayer.forEach((cp) => {
+              //   var value = cp.getAttribute("value");
+              //   if (value == -1) {
+              //     this.playerToLose++;
+              //     if (this.playerToLose === 17) {
+              //       header.innerText = "Computer wins!";
+              //       let timerInSeconds = 0;
+              //       setInterval(() => {
+              //         timerInSeconds += 1;
+
+              //         header.innerText = `Restart game in: ${timerInSeconds}`;
+
+              //         if (timerInSeconds >= 5) {
+              //           window.location.reload();
+              //         }
+              //       }, 1000);
+              //     } else if (this.playerToLose !== 17) {
+              //       this.playerToLose = 0;
+              //     }
+              //   }
+              // });
             } else {
               return;
             }
@@ -101,6 +124,7 @@ export class ShowBoard {
               cell.classList.add("isWata");
               cell.setAttribute("value", 2);
               const cellsPlayer = document.querySelectorAll(".cellPlayer");
+              attack();
               cellsPlayer.forEach((cp) => {
                 var value = cp.getAttribute("value");
                 if (value == -1) {
@@ -108,7 +132,6 @@ export class ShowBoard {
                   if (this.playerToLose === 17) {
                     header.innerText = "Computer wins!";
                     let timerInSeconds = 0;
-
                     setInterval(() => {
                       timerInSeconds += 1;
 
@@ -118,6 +141,8 @@ export class ShowBoard {
                         window.location.reload();
                       }
                     }, 1000);
+                  } else if (this.playerToLose !== 17) {
+                    this.playerToLose = 0;
                   }
                 }
               });
@@ -141,12 +166,16 @@ export class ShowBoard {
           cellPlayer.setAttribute("x", j);
           cellPlayer.setAttribute("value", 0);
           cellPlayer.addEventListener("click", (e) => {
-            y = parseInt(e.target.getAttribute("y"));
-            x = parseInt(e.target.getAttribute("x"));
-            player.fillBoard(y, x, playerBoard, switcher);
-            boardPlayer.innerHTML = "";
-            this.showBoardPlayer(playerBoard);
-            player.onesNum(playerBoard);
+            if (player.onesInBoard < 53) {
+              y = parseInt(e.target.getAttribute("y"));
+              x = parseInt(e.target.getAttribute("x"));
+              player.fillBoard(y, x, playerBoard, switcher);
+              boardPlayer.innerHTML = "";
+              this.showBoardPlayer(playerBoard);
+              player.onesNum(playerBoard);
+            } else {
+              return;
+            }
           });
         } else if (playerBoard.board[i][j] === 1) {
           const cellPlayer = document.createElement("div");
@@ -157,31 +186,61 @@ export class ShowBoard {
           cellPlayer.setAttribute("x", j);
           cellPlayer.setAttribute("value", 1);
           cellPlayer.addEventListener("click", (e) => {
-            y = parseInt(e.target.getAttribute("y"));
-            x = parseInt(e.target.getAttribute("x"));
-            player.fillBoard(y, x, playerBoard, switcher);
-            boardPlayer.innerHTML = "";
-            this.showBoardPlayer(playerBoard);
-            player.onesNum(playerBoard);
+            if (player.onesInBoard < 53) {
+              y = parseInt(e.target.getAttribute("y"));
+              x = parseInt(e.target.getAttribute("x"));
+              player.fillBoard(y, x, playerBoard, switcher);
+              boardPlayer.innerHTML = "";
+              this.showBoardPlayer(playerBoard);
+              player.onesNum(playerBoard);
+            } else {
+              return;
+            }
           });
         } else if (playerBoard.board[i][j] === 3) {
           const cellPlayer = document.createElement("div");
           cellPlayer.classList.add("cellPlayer");
-          cellPlayer.classList.add("isWata"); // show borders
+          // cellPlayer.classList.add("isWata"); // show borders
           boardPlayer.appendChild(cellPlayer);
           cellPlayer.setAttribute("value", 3);
           cellPlayer.setAttribute("y", i);
           cellPlayer.setAttribute("x", j);
           cellPlayer.addEventListener("click", (e) => {
-            y = parseInt(e.target.getAttribute("y"));
-            x = parseInt(e.target.getAttribute("x"));
-            player.fillBoard(y, x, playerBoard, switcher);
-            boardPlayer.innerHTML = "";
-            this.showBoardPlayer(playerBoard);
-            player.onesNum(playerBoard);
+            if (player.onesInBoard < 53) {
+              y = parseInt(e.target.getAttribute("y"));
+              x = parseInt(e.target.getAttribute("x"));
+              player.fillBoard(y, x, playerBoard, switcher);
+              boardPlayer.innerHTML = "";
+              this.showBoardPlayer(playerBoard);
+              player.onesNum(playerBoard);
+            } else {
+              return;
+            }
           });
         }
       }
     }
+  }
+}
+
+function getRandomInt() {
+  let max = 10;
+  return Math.floor(Math.random() * max);
+}
+
+function attack() {
+  let selector =
+    '.cellPlayer[y="' + getRandomInt() + '"][x="' + getRandomInt() + '"]';
+  let divToFind = document.querySelector(selector);
+  let atr = divToFind.getAttribute("value");
+  if (atr == 1) {
+    divToFind.classList.add("isHit");
+    divToFind.setAttribute("value", -1);
+    attack();
+  } else if (atr == 2) {
+    attack();
+  } else if (atr == 3 || atr == 0) {
+    divToFind.classList.add("isWata");
+    divToFind.setAttribute("value", 2);
   }
 }
